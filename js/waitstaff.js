f82e71dc67560ade35ee1264ee00a6180d872f3f
dxeware <dxeware@gmail.com>
2015-08-09 18:21:27 -0700

    Added cancel and reset button functionality

--- js/waitstaff.js
+++ js/waitstaff.js
@@ -64,58 +64,31 @@ angular.module('waitstaffCalc', [])
             $scope.avgTipStr = (tipCount / mealCount * 100).toFixed(2);
         }
     };
-    // Fill in the user's answers for story
-    $scope.fillIn = function() {
-        $scope.answers[0] = $scope.relative1;
-        $scope.answers[1] = $scope.adjective1;
-        $scope.answers[2] = $scope.adjective2;
-        $scope.answers[3] = $scope.adjective3;
-        $scope.answers[4] = $scope.person1;
-        $scope.answers[5] = $scope.adjective4;
-        $scope.answers[6] = $scope.person1;
-        $scope.answers[7] = $scope.adjective5;
-        $scope.answers[8] = $scope.verbed;
-        $scope.answers[9] = $scope.body;
-        $scope.answers[10] = $scope.verbing;
-        $scope.answers[11] = $scope.nounpl;
-        $scope.answers[12] = $scope.noun;
-        $scope.answers[13] = $scope.adverb;
-        $scope.answers[14] = $scope.verb1;
-        $scope.answers[15] = $scope.verb2;
-        $scope.answers[16] = $scope.hugenumber;
-        $scope.answers[17] = $scope.relative2;
-        $scope.answers[18] = $scope.person2;
 
+    $scope.cancel = function() {
 
+        $scope.price = "";
+        $scope.tax = "";
+        $scope.tip = "";
     };
 
     // Reset in order to start over
     $scope.startOver = function() {
-        $scope.showStory=false;
-        $scope.showInputs=true;
+        tipCount = 0;
+        mealCount = 0;
 
-        for (var i = 0; i < $scope.answers.length; i++) {
-            $scope.answers[i] = "";
-        }
+        //Clear inputs by calling cancel()
+        $scope.cancel();
 
-        $scope.relative1 = "";
-        $scope.adjective1 = "";
-        $scope.adjective2 = "";
-        $scope.adjective3 = "";
-        $scope.person1 = "";
-        $scope.adjective4 = "";
-        $scope.person1 = "";
-        $scope.adjective5 = "";
-        $scope.verbed = "";
-        $scope.body = "";
-        $scope.verbing = "";
-        $scope.nounpl = "";
-        $scope.noun = "";
-        $scope.adverb = "";
-        $scope.verb1 = "";
-        $scope.verb2 = "";
-        $scope.hugenumber = "";
-        $scope.relative2 = "";
-        $scope.person2 = "";
+        $scope.subtotalStr = "";
+        $scope.tipDollarsStr = "";
+        $scope.mealTotalStr = "";
+        $scope.tipCountStr = "";
+        $scope.mealCountStr = "";
+        $scope.avgTipStr = "";
+
     };
+
+
+
 });
